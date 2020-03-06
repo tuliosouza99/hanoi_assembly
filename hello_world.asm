a403249aa63add82d9a00e30dcf27e808e2076ae
Túlio Souza <49206513+tuliosouza99@users.noreply.github.com>
2020-03-06 10:26:00 -0300

    Add files via upload

--- hello_world.asm
+++ hello_world.asm
@@ -1,5 +1,6 @@
 .686
-.model flat, stdcall
+.model flat,stdcall
+
 option casemap:none
 
 include   /masm32/include/windows.inc
@@ -13,31 +14,25 @@ includelib /masm32/lib/masm32.lib
 
 .data
     msg1 db "Move da torre", 0h
-    msg2 db "para torre", 0h
-    msg3 db " ", 0ah, 0h
-    torre1 dd "1", 0h
-    torre2 dd "2", 0h
-    torre3 dd "3", 0h
-    aux dd ?
+    msg2 db "para a torre", 0h
+    quebra_linha db " ", 0ah, 0h ;quebra de linha
     
     write_count dd 0; Variavel para armazenar caracteres escritos na console
     var_write dd ?
-    
-    orig db 10 dup (?)
-    dest db 10 dup (?)
 
+    v_orig db 2 dup (?)
+    v_dest db 2 dup (?)
+ 
 .code
 
 start:
 
-    ; hanoi(n, torre1, torre2, torre3)
-    push 0                      ;base da pilha
-    push torre3                      ;torre3
-    push torre2                      ;torre2
-    push torre1                      ;torre1
-    push 3                      ;n discos
+    ; hanoi(n, origem, destino, aux)
+    push 2                      ;torre aux
+    push 3                      ;torre destino
+    push 1                      ;torre origem
+    push 4                      ;n discos
     call hanoi
-    add esp, 8                 ;limpa a pilha
     
     invoke ExitProcess, 0
 
@@ -46,15 +41,15 @@ hanoi:
     mov ebp, esp
     
     mov ebx, [ebp+8]           ;get n
-    mov esi, [ebp+12]          ;get torre1
-    mov edi, [ebp+16]          ;get torre2
-    mov ecx, [ebp+20]          ;get torre3
-    
+    mov esi, [ebp+12]          ;get torre origem
+    mov edi, [ebp+16]          ;get torre destino
+    mov ecx, [ebp+20]          ;get torre aux
+
     ; if(n == 1)
     cmp ebx, 1
     je exception
 
-    ; hanoi (n-1, torre1, torre3, torre2)
+    ; hanoi (n-1, origem, aux, destino)
     push edi
     push ecx
     push esi
@@ -62,46 +57,46 @@ hanoi:
     push ebx
     call hanoi
 
-    pop esi ;pop [ebp+12]
-    ;mov aux, esi
-    invoke atodw, addr torre1
-    invoke dwtoa, eax, addr orig
-    pop edi ;pop [ebp+16]
-    mov aux, edi
-    invoke atodw, addr aux
-    invoke dwtoa, eax, addr dest
+    pop ebx ;n
+    pop esi ;torre origem
+    pop edi ;torre destino
+    pop ecx ;torre aux
 
+    invoke dwtoa, esi, addr v_orig
+    invoke dwtoa, edi, addr v_dest
+     
     ; printf("\nMove o disco %d da torre %c para a torre %c", n, origem, destino)
+    
     invoke GetStdHandle, STD_OUTPUT_HANDLE
     mov var_write, eax
     invoke WriteConsole, var_write, addr msg1, sizeof msg1, addr write_count, NULL
 
-    ; printf("\nMove o disco %d da torre %c para a torre %c", n, origem, destino)
     invoke GetStdHandle, STD_OUTPUT_HANDLE
     mov var_write, eax
-    invoke WriteConsole, var_write, addr orig, sizeof orig, addr write_count, NULL
+    invoke WriteConsole, var_write, addr v_orig, sizeof v_orig, addr write_count, NULL
 
-    ; printf("\nMove o disco %d da torre %c para a torre %c", n, origem, destino)
     invoke GetStdHandle, STD_OUTPUT_HANDLE
     mov var_write, eax
     invoke WriteConsole, var_write, addr msg2, sizeof msg2, addr write_count, NULL
 
-    ; printf("\nMove o disco %d da torre %c para a torre %c", n, origem, destino)
     invoke GetStdHandle, STD_OUTPUT_HANDLE
     mov var_write, eax
-    invoke WriteConsole, var_write, addr dest, sizeof dest, addr write_count, NULL
+    invoke WriteConsole, var_write, addr v_dest, sizeof v_dest, addr write_count, NULL
 
-    ; printf("\nMove o disco %d da torre %c para a torre %c", n, origem, destino)
     invoke GetStdHandle, STD_OUTPUT_HANDLE
     mov var_write, eax
-    invoke WriteConsole, var_write, addr msg3, sizeof msg3, addr write_count, NULL
-    
-    ; hanoi(n-1, torre3, torre2, torre1)
+    invoke WriteConsole, var_write, addr quebra_linha, sizeof quebra_linha, addr write_count, NULL
 
+    push ecx ;torre aux
+    push edi ;torre destino
+    push esi ;torre origem
+    push ebx ;n
+       
+    ; hanoi(n-1, aux, destino, origem)
     mov ebx, [ebp+8]           ;get n
-    mov esi, [ebp+12]          ;get torre1
-    mov edi, [ebp+16]          ;get torre2
-    mov ecx, [ebp+20]          ;get torre3
+    mov esi, [ebp+12]          ;get torre origem
+    mov edi, [ebp+16]          ;get torre destino
+    mov ecx, [ebp+20]          ;get torre aux
     
     push esi
     push edi
@@ -113,39 +108,31 @@ hanoi:
     jmp quit
     
 exception:
-    
+
+    invoke dwtoa, esi, addr v_orig
+    invoke dwtoa, edi, addr v_dest
+     
     ; printf("\nMove o disco %d da torre %c para a torre %c", n, origem, destino)
+    
     invoke GetStdHandle, STD_OUTPUT_HANDLE
     mov var_write, eax
     invoke WriteConsole, var_write, addr msg1, sizeof msg1, addr write_count, NULL
 
-    ; printf("\nMove o disco %d da torre %c para a torre %c", n, origem, destino)
     invoke GetStdHandle, STD_OUTPUT_HANDLE
     mov var_write, eax
-    invoke WriteConsole, var_write, addr orig, sizeof orig, addr write_count, NULL
+    invoke WriteConsole, var_write, addr v_orig, sizeof v_orig, addr write_count, NULL
 
-    ; printf("\nMove o disco %d da torre %c para a torre %c", n, origem, destino)
     invoke GetStdHandle, STD_OUTPUT_HANDLE
     mov var_write, eax
     invoke WriteConsole, var_write, addr msg2, sizeof msg2, addr write_count, NULL
 
-    ; printf("\nMove o disco %d da torre %c para a torre %c", n, origem, destino)
     invoke GetStdHandle, STD_OUTPUT_HANDLE
     mov var_write, eax
-    invoke WriteConsole, var_write, addr dest, sizeof dest, addr write_count, NULL
+    invoke WriteConsole, var_write, addr v_dest, sizeof v_dest, addr write_count, NULL
 
-    ; printf("\nMove o disco %d da torre %c para a torre %c", n, origem, destino)
     invoke GetStdHandle, STD_OUTPUT_HANDLE
     mov var_write, eax
-    invoke WriteConsole, var_write, addr msg3, sizeof msg3, addr write_count, NULL
-    
-kpop:
-    pop [ebp+8]
-    pop [ebp+12]
-    pop [ebp+16]
-    pop [ebp+20]
-
-
+    invoke WriteConsole, var_write, addr quebra_linha, sizeof quebra_linha, addr write_count, NULL
 
 quit:
     mov esp, ebp
